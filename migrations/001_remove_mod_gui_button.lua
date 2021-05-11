190937ea25a07f214ce4b9eff02ffd2bf1b2ffc6
Andreas Svanberg <andreas.svanberg@mensa.se>
2021-05-11 10:26:41 +0200

    Fixed migration on saves from Factorio 1.1.0 or later.

--- migrations/001_remove_mod_gui_button.lua
+++ migrations/001_remove_mod_gui_button.lua
@@ -1,8 +1,10 @@
 local mod_gui = require("mod-gui")
 for _, player in pairs(game.players) do
-  local button_flow = player.gui.top.mod_gui_button_flow
-  local pp_button = button_flow["pp-toggle-gui"]
-  if pp_button and pp_button.valid then
-    pp_button.destroy()
+  local button_flow = mod_gui.get_button_flow(player)
+  if button_flow and button_flow.valid then
+    local pp_button = button_flow["pp-toggle-gui"]
+    if pp_button and pp_button.valid then
+      pp_button.destroy()
+    end
   end
 end
